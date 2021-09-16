70e17e316f5ad3711742909372cc27c237cbc89f
Ryam Sousa <ryam.sousa123@gmail.com>
2021-09-15 22:16:01 -0300

    Initial commit

---
+++ src/main/java/com/personal_finances/PersonalFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.personal_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinancesApplication.class, args);
+	}
+
+}
