36fa4a74def8ff5386767400016e4c82ce88f3d4
Ryam Sousa <ryam.sousa123@gmail.com>
2021-09-18 17:24:09 -0300

    Feat: create documentation

--- src/main/java/com/personal_finances/controller/RevenuesController.java
+++ src/main/java/com/personal_finances/controller/RevenuesController.java
@@ -4,6 +4,8 @@ import com.personal_finances.model.dto.CategoriesDTO;
 import com.personal_finances.model.dto.RevenuesDTO;
 import com.personal_finances.service.CategoriesService;
 import com.personal_finances.service.RevenuesService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Api("Retorna dados das receitas")
 @RestController
 @RequestMapping(value = "/revenues")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -21,26 +24,31 @@ public class RevenuesController {
 
     private final RevenuesService serviceRevenue;
 
+    @ApiOperation("Cria uma nova receita")
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<RevenuesDTO> save(@Valid @RequestBody RevenuesDTO dto){
         return ResponseEntity.ok(serviceRevenue.save(dto));
     }
 
+    @ApiOperation("Busca uma receita por id")
     @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<RevenuesDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(serviceRevenue.findById(id));
     }
 
+    @ApiOperation("Busca uma receita por categoria")
     @GetMapping(value = "/category/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RevenuesDTO>> findByCategory(@PathVariable Long id){
         return ResponseEntity.ok(serviceRevenue.findByCategory(id));
     }
 
+    @ApiOperation("Deleta uma receita por id")
     @DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<RevenuesDTO> delete(@PathVariable Long id){
         return ResponseEntity.ok(serviceRevenue.delete(id));
     }
 
+    @ApiOperation("Busca todas as receitas")
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RevenuesDTO>> findAllRevenues(){
         return ResponseEntity.ok(serviceRevenue.findAllRevenues());
--- src/main/java/com/personal_finances/controller/UsersController.java
+++ src/main/java/com/personal_finances/controller/UsersController.java
@@ -1,9 +1,9 @@
 package com.personal_finances.controller;
 
-import com.personal_finances.model.dto.RevenuesDTO;
 import com.personal_finances.model.dto.UsersDTO;
-import com.personal_finances.service.RevenuesService;
 import com.personal_finances.service.UsersService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +14,7 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Api("Retorna dados dos usuários")
 @RestController
 @RequestMapping(value = "/users")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -21,26 +22,31 @@ public class UsersController {
 
     private final UsersService usersService;
 
+    @ApiOperation("Cria um novo usuário")
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<UsersDTO> save(@Valid @RequestBody UsersDTO dto){
         return ResponseEntity.ok(usersService.save(dto));
     }
 
+    @ApiOperation("Busca um usuário por id")
     @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<UsersDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok(usersService.findById(id));
     }
 
+    @ApiOperation("Busca um usuário por cpf")
     @GetMapping(value = "/category/{cpf}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<UsersDTO> findByCpf(@PathVariable String cpf){
         return ResponseEntity.ok(usersService.findByCpf(cpf));
     }
 
+    @ApiOperation("Deleta um usuário por id")
     @DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<UsersDTO> delete(@PathVariable Long id){
         return ResponseEntity.ok(usersService.delete(id));
     }
 
+    @ApiOperation("Busca todos os usuário")
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<UsersDTO>> findAllUsers(){
         return ResponseEntity.ok(usersService.findAllUsers());
