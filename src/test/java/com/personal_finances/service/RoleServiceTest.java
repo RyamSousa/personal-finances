b3bc27f9e3b5f946e4199489b0006ee9164a1e14
Ryam Sousa <ryam.sousa123@gmail.com>
2022-01-14 16:13:14 -0300

    create test delete role

--- src/test/java/com/personal_finances/service/RoleServiceTest.java
+++ src/test/java/com/personal_finances/service/RoleServiceTest.java
@@ -2,6 +2,8 @@ package com.personal_finances.service;
 
 import com.personal_finances.model.Role;
 import com.personal_finances.model.RoleMother;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.BDDAssertions;
 import com.personal_finances.repository.RoleRepository;
@@ -28,7 +30,7 @@ public class RoleServiceTest {
     private RoleService roleService;
 
     @Test
-    void givenRequestValid_whenCreateRole_thenCreateAndReturnUser(){
+    void givenRequestValid_whenCreateRole_thenCreateAndReturnRole(){
         Role role = RoleMother.getRole();
 
         given(roleRepository.save(any(Role.class))).willReturn(role);
@@ -38,4 +40,26 @@ public class RoleServiceTest {
 
         BDDAssertions.assertThat(save).usingRecursiveComparison().isEqualTo(role);
     }
+
+    @Test
+    void givenRequestValid_whenDeleteRole_thenDeleteAndReturnRole(){
+        Role role = RoleMother.getRole();
+
+        given(roleRepository.findByName(any(String.class))).willReturn(Optional.of(role));
+        Role delete = roleService.delete(role.getName());
+
+        BDDAssertions.assertThat(delete).usingRecursiveComparison().isEqualTo(role);
+    }
+
+    @Test
+    void givenRequestValid_whenFindRoleByName_thenGetAndReturnRole(){
+        Role role = RoleMother.getRole();
+
+        given(roleRepository.findByName(any(String.class))).willReturn(Optional.of(role));
+
+        Role roleFinded = roleService.findByName(role.getName());
+
+        BDDAssertions.assertThat(role).usingRecursiveComparison().isEqualTo(roleFinded);
+    }
+
 }
